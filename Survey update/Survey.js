bf17eb0f968d13e8343bf9042a179cf169347a4b
Кондратюк Георгій <71070022+Code-Xaos@users.noreply.github.com>
2020-10-16 12:56:13 +0300

    Update Survey.js

--- Survey update/Survey.js
+++ Survey update/Survey.js
@@ -1,13 +1,13 @@
 window.onload = () => {
     const EMAIL_REGEXP = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
     const INPUT = document.getElementById('email');
-    var $error = $(".error");
+
     function validateEmail(value) {
       return EMAIL_REGEXP.test(value);
     }
     
     function updateInput() {
-        if (!validateEmail(INPUT.value)){ INPUT.style.borderColor = 'red'; $error.fadeIn();
+        if (!validateEmail(INPUT.value)){ INPUT.style.borderColor = 'red';
     }else{ INPUT.style.borderColor = 'green'; }
     
     }
